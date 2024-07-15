7ddeed869cdad12e2ae32307a7eb7092477761fb
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-07-15 22:44:43 +0500

    feat: add drawing function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,40 @@
+"use client";
+
+import { useDraw } from "@/hooks/useDraw";
 import { FC } from "react";
 
 interface pageProps {}
 
 const page: FC<pageProps> = ({}) => {
+  const { canvasRef, onMouseDown } = useDraw(drawLine);
+
+  function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
+    const { x: currX, y: currY } = currentPoint;
+    const lineColor = "#000";
+    const lineWidth = 5;
+
+    let startPoint = prevPoint ?? currentPoint;
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = lineColor;
+    ctx.moveTo(startPoint.x, startPoint.y);
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = lineColor;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 2, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
   return (
     <div className="w-screen h-screen bg-white flex justify-center items-center">
       <canvas
         className="border border-black rounded-md"
-        width={750}
-        height={750}
+        width={550}
+        height={550}
+        ref={canvasRef}
+        onMouseDown={onMouseDown}
       />
     </div>
   );
